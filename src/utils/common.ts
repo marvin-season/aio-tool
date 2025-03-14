b9a090ee1d0f8d95a9ea5f6db42f999896e86944
marvin <sea-son@foxmail.com>
2025-03-14 14:36:01 +0800

    feat: createIntersectionObserver

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -5,3 +5,31 @@ export async function sleep(timeout: number) {
 export function sum(a: number, b: number) {
     return a + b;
 }
+
+export function createIntersectionObserver({
+    options,
+    onIntersecting,
+    targets = [],
+    callback
+}: {
+    options: IntersectionObserverInit;
+    targets: HTMLElement[];
+    onIntersecting: (entries: IntersectionObserverEntry[]) => void;
+    callback?: IntersectionObserverCallback
+}) {
+    const observer = new IntersectionObserver((entries) => {
+        const intersectionObserverEntries = entries.filter(
+            (item) => item.isIntersecting,
+        );
+        if (intersectionObserverEntries.length > 0) {
+            onIntersecting(intersectionObserverEntries);
+        }
+        callback && callback(entries, observer);
+    }, options);
+
+    targets.forEach((target) => {
+        observer.observe(target);
+    });
+
+    return observer;
+}
